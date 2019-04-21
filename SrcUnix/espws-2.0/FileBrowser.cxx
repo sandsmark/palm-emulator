196aca5b12b928a5967f6fe19b7759e1676fc7c2
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2019-04-21 17:39:47 +0200

    alpha sort file dialog

--- SrcUnix/espws-2.0/FileBrowser.cxx
+++ SrcUnix/espws-2.0/FileBrowser.cxx
@@ -458,7 +458,7 @@ FileBrowser::load(const char *directory)// I - Directory to load
 
     num_files = fl_filename_list(filename, &files);
 #else
-    num_files = scandir(directory_, &files, nullptr, nullptr);
+    num_files = scandir(directory_, &files, nullptr, alphasort);
 #endif /* WIN32 || __EMX__ */
 
     if (num_files <= 0)
